b55392e871684e4fe3b3c093c6913af238167fa5
custodio-prog <56941184+custodio-prog@users.noreply.github.com>
2020-07-07 18:24:29 -0300

    final version

---
+++ PyTorch/plotter.py
@@ -0,0 +1,28 @@
+import matplotlib
+import matplotlib.pyplot as plt  
+import torch
+
+def plot(values, moving_avg_period):
+    plt.figure(2)
+    plt.clf()
+    plt.title('Training...')
+    plt.xlabel('Episode')
+    plt.ylabel('Duration')
+    plt.plot(values)
+    plt.plot(get_moving_average(moving_avg_period, values))
+    plt.pause(0.001)
+    print("Episode", len(values), "\n", \
+        moving_avg_period, "episode moving avg:", moving_avg[-1])
+
+    
+def get_moving_average(period, values):
+    values = torch.Tensor(values, dtype = torch.float)
+    if len(values) >= period:
+        moving_avg = values.unfold(dimension=0, size=period, step=1) \
+            .mean(dim=1).flatten(start_dim=0)
+        moving_avg = torch.cat((torch.zeros(period-1), moving_avg))
+        return moving_avg.numpy()
+    else:
+        moving_avg = torch.zeros(len(values))
+        return moving_avg.numpy()
+    
